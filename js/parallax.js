466e7c618a71d782b66053dd30873b14b68dcff7
jamestariga <jamestariga08@live.com>
2021-11-26 16:20:06 -0800

    Major update

--- js/parallax.js
+++ js/parallax.js
@@ -1,33 +1,21 @@
-let stars = document.getElementByID('stars');
-let moon = document.getElementByID('moon');
-let mountains_behind = document.getElementByID('mountains_behind');
-let mountains_front = document.getElementByID('mountains_front');
-let text = document.getElementByID('text');
-let btn = document.getElementByID('btn');
-
-window.addEventListener('scroll', function(){
-  let value = window.scrollY;
-  stars.style.left = value * 0.25 + 'px';
-  moon.style.top = value * 1.05 + 'px';
-  mountains_behind.style.top = value * 0.5 + 'px';
-  mountains_front.style.top = value * 0 + 'px';
-  text.style.marginRight = value * 4 + 'px';
-});
-
 const parallaxEffect = () => {
-  const stars = document.getElementByID('stars');
-  const moon = document.getElementByID('moon');
-  const mountains_behind = document.getElementByID('mountains_behind');
-  const text = document.getElementByID('text');
-  const btn = document.getElementByID('btn');
-  const mountains_front = document.getElementByID('mountains_front');
+    const moon = document.getElementById('moon');
+    const stars = document.getElementById('stars');
+    const mountains_behind = document.getElementById('mountains_behind');
+    const mountains_front = document.getElementById('mountains_front');
+    const text = document.getElementById('text');
+    const btn = document.getElementById('btn');
+    const header = document.querySelector('header');
 
-  window.addEventListener('scroll', function(){
-    let value = window.scrollY;
+  window.addEventListener('scroll', () => {
+    var value = window.scrollY;
+    moon.style.top = -value * -1.05 + 'px';
     stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
-    mountains_behind.style.top = value * 0.5 + 'px';
-    mountains_front.style.top = value * 0 + 'px';
+    mountains_behind.style.top =-value * -0.5 + 'px';
+    header.style.top =-value * -0.5 + 'px';
+    mountains_front.style.top =-value * 0 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
     text.style.marginRight = value * 4 + 'px';
   });
 
